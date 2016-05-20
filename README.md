0da84f5aad652b1ea39002b3de6b18138f4c0436
CJ R <hello@cjr.co.de>
2016-05-19 21:31:05 -0700

    moar better

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # git-fly
 
-A command line tool to temporarily and easily clone and run repos on the fly.
+A command line tool to temporarily clone, run and open git repos on the fly.
+
+To get started install git-fly globally:
+
+`npm install -g git-fly`
+
+Then run the command:
+
+`git-fly`
+
+### git-fly asks for 3 things:
+
+#### repo URL
+(required) This is the URL of the repo to clone.
+
+#### run command
+(optional) The command to run after cloning the repo.
+
+For example: http-server, npm start etc.
+
+#### open command
+(optional) The command to open the cloned repo directory.
+
+For example: atom, open etc.
+
+If a package.json file is detected, git-fly will offer to run npm install.
--- index.js
+++ index.js
@@ -5,8 +5,13 @@
 const inquirer = require('inquirer');
 const safename = require('safename');
 
+const fs = require('fs');
 const process = require('process');
 const spawn = require('child_process').spawn;
+
+const stdin = process.openStdin();
+process.stdin.setRawMode(true);
+stdin.resume();
 
 const GitCommandLine = require('git-command-line');
 const Git = new GitCommandLine();
@@ -14,96 +19,216 @@ const Git = new GitCommandLine();
 const StringDecoder = require('string_decoder').StringDecoder;
 const decoder = new StringDecoder('utf8');
 
-process.on('SIGINT', function() {
-  if(running){
-    running = false;
-    if(cleanup) {
-      cleanup();
-    }
-    askAgain();
-  } else {
-    console.log('Exiting process');
-    process.exit();
-  }
-});
-
+let running = false;
+let cleanupPaths = [];
+let currentCleanup = 0;
 let questions = [{
   type: 'input',
   name: 'repo',
-  message: 'What repo would you like to clone?'
+  message: 'repo URL (required):',
+  validate: function(input) {
+    return input ? true : 'You must enter a repo URL';
+  }
 }, {
   type: 'input',
   name: 'run',
-  message: 'How do you want to run it?'
+  message: 'run command (optional)(e.g. http-server, npm start):'
 }, {
-  type: 'confirm',
+  type: 'input',
   name: 'open',
-  message: 'Would you like to open the repo in atom?'
+  message: 'open command (optional)(e.g. atom, subl):'
 }];
 
-let running = false;
-let cleanup;
+function start() {
+  console.log('Welcome to git-fly');
+  console.log('Temporarily clone/run/open git repos on the fly');
+  currentCleanup = 0;
 
-function ask() {
   inquirer.prompt(questions).then(function(answers) {
     let safePath = '/tmp/' + safename(answers.repo);
-    console.log('Cloning repo into ' + safePath);
     running = false;
-    cleanup = null;
-    Git.clone(answers.repo + ' ' + safePath).then(function(res) {
-      running = true;
 
-      if(answers.open) {
-        spawn('atom', [safePath]);
+    function localRunIt() {
+      if (answers.run) {
+        running = true;
+        runIt(answers.run, safePath);
+      } else {
+        startAgain();
       }
+    }
 
-      cleanup = function() {
-        console.log('Cleaning up...');
-        spawn('rm', ['-rf', safePath]);
-      }
+    function checkPackageJSON() {
+      fs.access(safePath + '/package.json', fs.F_OK, function(err) {
+        if (!err) {
+          inquirer.prompt([{
+            type: 'confirm',
+            name: 'install',
+            message: 'package.json detected! run npm install?',
+            default: false
+          }]).then(function(answers) {
+            if (answers.install) {
+              const install = spawn('npm', ['install'], {
+                cwd: safePath
+              });
+              console.log('Running npm install...')
+              setupLog(install, 'Finished installing!', function() {
+                localRunIt();
+              });
 
-      let runWithParams = answers.run.split(' ');
-      let command = runWithParams[0];
-      let params = runWithParams.slice(1, runWithParams.length - 1)
+            } else {
+              localRunIt();
+            }
+          })
+        } else {
+          localRunIt();
+        }
+      })
+    }
 
-      const run = spawn(command, params, {cwd: safePath});
+    function cloneRepo() {
+      console.log('Cloning repo into ' + safePath);
+      Git.clone(answers.repo + ' ' + safePath).then(function(res) {
+        if (answers.open) {
+          spawn(answers.open, [safePath]);
+        }
 
-      run.stdout.on('data', (data) => {
-        if(running) {
-          console.log(decoder.write(data));
+        if(cleanupPaths.indexOf(safePath) == -1) {
+          cleanupPaths.push(safePath);
+        }
+
+        checkPackageJSON();
+
+      }).catch(function(err) {
+        if (err.stderr.indexOf("already exists") > -1) {
+          inquirer.prompt([{
+            type: 'confirm',
+            name: 'remove',
+            message: safePath + ' already exists. Remove it and clone again?',
+            default: false
+          }]).then(function(answers) {
+            if (answers.remove) {
+              console.log('Removing ' + safePath + '...');
+              cleanupPath(safePath, function () {
+                cloneRepo();
+              });
+            } else {
+              checkPackageJSON();
+            }
+          });
+        } else {
+          console.error('Error cloning repo:', err.stderr || err);
+          startAgain();
         }
       });
+    }
 
-      run.stderr.on('data', (data) => {
-        if(running) {
-          console.error(decoder.write(data));
-        }
-      });
-
-    }).catch(function(err) {
-      console.error('Failed to clone the repo with the following error:', err.stderr || err);
-      askAgain();
-    });
+    cloneRepo();
   });
 }
 
-function askAgain() {
+function cleanupPath(path, cb) {
+  let rm = spawn('rm', ['-rf', path]);
+  setupLog(rm, '', cb);
+}
+
+function cleanup(done) {
+  if (cleanupPaths.length > 0 && currentCleanup == 0) {
+    console.log('Starting cleanup...');
+  } else {
+    console.log('So long and thanks for all the fish.');
+    return process.exit();
+  }
+
+  if(currentCleanup < cleanupPaths.length) {
+    var path = cleanupPaths[currentCleanup];
+    inquirer.prompt([{
+      type: 'confirm',
+      name: 'cleanup',
+      message: 'Remove ' + path + '?',
+      default: true
+    }]).then(function(answers) {
+      if(answers.cleanup) {
+        cleanupPath(path, function(){
+          console.log('Removed ' + path);
+          currentCleanup++;
+          cleanup();
+        })
+      }
+    });
+  } else if (cleanupPaths.length > 0) {
+    console.log('Finished cleaning up!');
+    console.log('So long and thanks for all the fish.');
+    process.exit();
+  }
+}
+
+function runIt(runCommand, path) {
+  if (runCommand) {
+    console.log('Running...');
+
+    let params = runCommand.split(' ');
+    let command = params[0];
+    params.shift();
+
+    const run = spawn(command, params, {
+      cwd: path
+    });
+
+    setupLog(run, '', function() {
+      console.log('Finished running.');
+      startAgain();
+    });
+  }
+}
+
+function setupLog(child, finishMessage, next) {
+  child.stdout.on('data', (data) => {
+    if (running) {
+      console.log(decoder.write(data));
+    }
+  });
+
+  child.stderr.on('data', (data) => {
+    if (running) {
+      console.error(decoder.write(data));
+    }
+  });
+
+  if (finishMessage || next) {
+    child.on('close', (code) => {
+      if (finishMessage) console.log(finishMessage);
+      if (next) next();
+    });
+  }
+
+}
+
+function startAgain() {
   inquirer.prompt([{
     type: 'confirm',
-    name: 'askAgain',
-    message: 'Want to clone another repo?',
+    name: 'startAgain',
+    message: 'Clone another repo?',
     default: true
   }]).then(function(answers) {
-    if (answers.askAgain) {
-      ask();
+    if (answers.startAgain) {
+      start();
     } else {
-      if(cleanup) {
-        cleanup();
-      }
-      console.log('See you later!');
-      process.exit();
+      cleanup();
     }
   })
 }
 
-ask();
+process.on('SIGINT', function() {
+  if (running) {
+    running = false;
+
+    if (cleanupPaths.length > 0) {
+      cleanup();
+    }
+    startAgain();
+  } else {
+    process.exit();
+  }
+});
+
+start();
